20fd59ad572bccde19db87600135bbace2deab3d
fbarrosalcalde <53537582+fbarrosalcalde@users.noreply.github.com>
2020-05-19 17:10:23 -0400

    opti.py

--- opti.py
+++ opti.py
@@ -14,8 +14,8 @@ Co = 5 #costo orujo por kg
 Ca = 3 #costo agua por litro -> Invenatdo
 Cl = 3 #costo electricidad por kw -> Inventado
 P = 1000 #precio agente reductor por kg -> Inventado
-D = 100 #demanda -> Inventado
-
+D = 1000000 #demanda -> Inventado
+M = 100000000000000000000 #número muy grande
 #cantidad agente reductor que se obtiene por kg con aditivo i, g/kg
 alpha = {'aditivo1':22, 'aditivo2':106, 'aditivo3': 85, 'aditivo4':101}
 #tiempo del ciclo aditivo i, dias
@@ -30,7 +30,7 @@ cc = {'cong1':300, 'cong2':396, 'cong3':490}
 epsilon = {'cong1':420, 'cong2':528, 'cong3':624}
 
 #creamos modelo
-m = Model('Produccion de agente reductor')
+m = Model('Producción de agente reductor')
 
 #crean y rellenan varibles de desicion
 # deben seguir el orden de los indices propuesto en el modelo
@@ -38,13 +38,13 @@ x = m.addVars(T,I,vtype = GRB.CONTINUOUS,name = 'orujo_fresco')
 y = m.addVars(T,I,C, vtype = GRB.CONTINUOUS,name = 'orujo_congelado')
 j = m.addVars(T, vtype=GRB.CONTINUOUS, name = 'compra')
 z = m.addVars(T,C,vtype=GRB.CONTINUOUS, name = 'congelar')
-b = m.addVars(T,I,vtype = GRB.BINARY, name='usar_ad')
+b = m.addVars(T,I,lb = 0.0, ub = 1.0, obj = 1.0,vtype = GRB.BINARY, name='usar_aditivo')
 
 #Variables auxiliares, no supe como se definian asi que las defini como variables.
 IF = m.addVars(T,vtype=GRB.CONTINUOUS,name='almacen')
-IC = m.addVars(T,C,vtype=GRB.CONTINUOUS,name = 'almacen_cong')
-a = m.addVars(T,vtype=GRB.BINARY,name='termino')
-w = m.addVars(C,T,vtype=GRB.BINARY,name='usando_cong')
+IC = m.addVars(T,C,vtype=GRB.CONTINUOUS,name = 'almacen_congelador')
+a = m.addVars(T,lb = 0.0, ub = 1.0, obj = 1.0,vtype=GRB.BINARY,name='termino')
+w = m.addVars(T,C,lb = 0.0, ub = 1.0, obj = 1.0,vtype=GRB.BINARY,name='usando_congelador')
 
 # se debe actualizar el modelo luego de agregar variables
 m.update()
@@ -109,6 +109,55 @@ for t in T:
     for c in C:
         for i in I:
             m.addConstr(((1 + delta[i]) * (x[t, i] + y[t, i, c])) <= 0.8 * Q, "capacidadreactor_{}_{}_{}".format(t, i, c))
-                        
-      
-            
+
+#Restricción solo se puede usar aditivo i si b[t, i] es distinto de cero
+for t in T:
+    for c in C:
+        for i in I:
+            m.addConstr(x[t, i] + y[t, i, c] <= M * b[t, i], "restriccionusoaditivoi_{}_{}_{}".format(t, i, c))
+
+#Restricción solo un aditivo en cada proceso
+for t in T:
+    cantidad_aditivos = 0
+    for i in I:
+        cantidad_aditivos += b[t, i]
+    m.addConstr(cantidad_aditivos <= 1, "restriccioncantidadaditivos_{}".format(t))
+
+#Restricción congeladores en uso
+for t in T:
+    for c in C:
+        m.addConstr(M * (cc[c] - IC[t,c]) >= w[t, c], "restriccion1congeladoresenuso_{}_{}".format(t, c))
+for t in T:
+    for c in C:
+        m.addConstr((cc[c] - IC[t,c]) / cc[c] <= w[t, c], "restriccion2congeladoresenuso_{}_{}".format(t, c))
+
+#Restricción término de tiempo en el último ciclo
+for t in T:
+    for i in I:
+        m.addConstr(b[t, i] * (T.index(t) + 1 + betha[i]) <= T.index(T[-1]) + 1, "restriccionterminociclo_{}_{}".format(t, i))
+
+#Restricción variable a[t]
+
+
+#Restricción los procesos solo comienzan el día después de terminar el anterior
+
+
+# Funcion objetivo
+obj = 0
+for t in T:
+    for c in C:
+        for i in I:
+            costos = 1.04 * (j[t] * Co + a[t] * (Ca + Cl) + w[t, c] * epsilon[c] * Cl + gama[i] * delta[i] * (x[t, i] + y[t, i, c]))
+            obj += ((P * alpha[i] * (x[t, i] + y[t, i, c])) - costos)
+        
+m.setObjective(obj, GRB.MAXIMIZE)
+
+m.optimize()
+
+# Mostrar los valores de las soluciones
+m.printAttr("X")
+
+#print("\n-------------\n")
+# Imprime las holguras de las restricciones (0 significa que la restricción es activa).
+#for constr in m.getConstrs():
+    #print(constr, constr.getAttr("slack"))
